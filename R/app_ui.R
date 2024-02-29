8875334147027b5d3b05a117dcf54ab7de4c78f7
Ivan Millanes <ivanmillanes26@gmail.com>
2024-02-29 10:20:36 -0300

    Extract logic to function

--- R/app_ui.R
+++ R/app_ui.R
@@ -17,13 +17,13 @@ app_ui <- function(request) {
         shiny::selectInput(
           inputId = "state",
           label = "Select State",
-          choices = sales_data$STATE |> unique() |> sort()
+          choices = get_choices(sales_data, "STATE")
         ),
 
         shiny::selectInput(
           inputId = "product_line",
           label = "Select Product Line",
-          choices = sales_data$PRODUCTLINE |> unique() |> sort()
+          choices = get_choices(sales_data, "PRODUCTLINE")
         ),
 
         shiny::actionButton(
