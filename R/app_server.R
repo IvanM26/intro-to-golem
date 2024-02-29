45269808bd81440c4fecf68f6825fbb98f98c6b4
Ivan Millanes <ivanmillanes26@gmail.com>
2024-02-29 10:54:56 -0300

    Extract filter functionality to module

--- R/app_server.R
+++ R/app_server.R
@@ -6,14 +6,7 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
-  rctv_data_filtered <- shiny::reactive({
-    sales_data |>
-      dplyr::filter(
-        STATE == input$state,
-        PRODUCTLINE == input$product_line
-      )
-  }) |>
-    shiny::bindEvent(input$apply)
+  rctv_data_filtered <- mod_filters_server("filters_1")
 
   output$chart_sales_by_city <- echarts4r::renderEcharts4r({
     create_chart(rctv_data_filtered())
---
+++ R/mod_filters.R
@@ -0,0 +1,59 @@
+#' filters UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_filters_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+
+    shiny::selectInput(
+      inputId = ns("state"),
+      label = "Select State",
+      choices = get_choices(sales_data, "STATE")
+    ),
+
+    shiny::selectInput(
+      inputId = ns("product_line"),
+      label = "Select Product Line",
+      choices = get_choices(sales_data, "PRODUCTLINE")
+    ),
+
+    shiny::actionButton(
+      inputId = ns("apply"),
+      label = "Apply"
+    )
+
+  )
+}
+
+#' filters Server Functions
+#'
+#' @noRd
+mod_filters_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+    rctv_data_filtered <- shiny::reactive({
+      sales_data |>
+        dplyr::filter(
+          STATE == input$state,
+          PRODUCTLINE == input$product_line
+        )
+    }) |>
+      shiny::bindEvent(input$apply)
+
+    return(rctv_data_filtered)
+
+  })
+}
+
+## To be copied in the UI
+# mod_filters_ui("filters_1")
+
+## To be copied in the server
+# mod_filters_server("filters_1")
