f1cd4518cc555f18abe415b73d644076e445893e
Ivan Millanes <ivanmillanes26@gmail.com>
2024-02-29 10:35:37 -0300

    Add server functionality

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,21 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  rctv_data_filtered <- shiny::reactive({
+    sales_data |>
+      dplyr::filter(
+        STATE == input$state,
+        PRODUCTLINE == input$product_line
+      )
+  }) |>
+    shiny::bindEvent(input$apply)
+
+  output$chart_sales_by_city <- echarts4r::renderEcharts4r({
+    create_chart(rctv_data_filtered())
+  })
+
+  output$table_orders <- reactable::renderReactable({
+    rctv_data_filtered() |>
+      reactable::reactable(width = "100%")
+  })
 }
